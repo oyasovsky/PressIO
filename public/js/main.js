0d54e1343a342eef89b2e1a4b54415af5bff95bd
Olga Yasovsky <oyasovsky@interwise.com>
2016-02-20 12:04:14 -0600

    fix saveAudio saveVideo menus

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var colors = ['#1abc9c',  '#57d68d', '#5cace2', '#9b59b6', '#f1c40f', '#e67e22', '#e74e60']
 angular.module('pressIO', [])
-	.controller('RSSTagsController', ['$scope', function ($scope) {
+	.controller('RSSTagsController', ['$scope', '$http', function ($scope, $http) {
 	$scope.rssFeeds = [
       	"David Bowie",
       	"Turkey Bombing",
@@ -13,7 +13,14 @@ angular.module('pressIO', [])
 		var i = (index>=colors.length) ? index % colors.length : index;
 		return colors[i]; 
 	};
+	
+	$scope.saveAudio = function() {
+		console.log("in save audio");
+	};
 
+	$scope.saveVideo = function() {
+		console.log("in save video");
+	};
 
   }]);
 
