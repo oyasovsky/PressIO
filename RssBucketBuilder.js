52151278f8176d9a5eb44be91e6566a132dc77a0
danperetz <dan.peretz@gmail.com>
2016-02-20 13:58:39 -0600

    with data

--- RssBucketBuilder.js
+++ RssBucketBuilder.js
@@ -1,8 +1,79 @@
 var feed = require('feed-read');
+var WordPOS = require('wordpos');
+var wordpos= new WordPOS();
+var deasync = require('deasync');
+
 var rssFeedsArray = [['http://rss.cnn.com/rss/edition.rss',require('./cnnParser')]];
 
+var data = {};
+var doneParseRss = false;
+var waitForFunctionParseRss = function() { return !doneParseRss; };
 function paragraphsFormater(pharagraphs) {
 	return pharagraphs;
+}
+
+function returnSortedLowerCaseArray(rawArray) {
+	return rawArray.map(function(x) { return x.toLowerCase(); }).sort();
+}
+
+function deleteSimilarWords(array1,array2) {
+	deleteIndex =[];
+	for (var i = 0; i < array1.length; i++) {
+		for (var k = 0; k < array2.length; k++) {
+			if (array1[i] === array2[k]) deleteIndex.push(k);
+		}
+	}
+
+	for (var j = deleteIndex.length - 1; j >= 0; j--) {
+		array2.splice(deleteIndex[j], 1);
+	}
+
+	return array2;
+
+}
+function deleteWrongWords(array) {
+	var result = [];
+	for (var i = array.length - 1; i >= 0; i--) {
+		if (array[i].length > 1) {
+			result.push(array[i]);
+		}
+	}
+	return result;
+}
+
+function refineArray(result) {
+	result.verbs = deleteSimilarWords(result.nouns,result.verbs);
+	result.adjectives = deleteSimilarWords(result.nouns,result.adjectives);
+	result.adverbs = deleteSimilarWords(result.nouns,result.adverbs);
+	result.rest = deleteSimilarWords(result.nouns,result.rest);
+
+        result.adjectives = deleteSimilarWords(result.verbs,result.adjectives);
+        result.adverbs = deleteSimilarWords(result.verbs,result.adverbs);   
+        result.rest = deleteSimilarWords(result.verbs,result.rest); 
+
+        result.adverbs = deleteSimilarWords(result.adjectives,result.adverbs);
+        result.rest = deleteSimilarWords(result.adjectives,result.rest); 
+
+        result.adverbs = deleteSimilarWords(result.rest,result.adverbs);
+
+	result.nouns= deleteWrongWords(result.nouns);
+	result.verbs= deleteWrongWords(result.verbs);
+	result.adverbs= deleteWrongWords(result.adverbs);
+	result.adjectives= deleteWrongWords(result.adjectives);
+	result.rest= deleteWrongWords(result.rest);
+
+	return result;
+}
+
+
+var getRssParsedResult = function(result) {
+	result.nouns=returnSortedLowerCaseArray(result.nouns);
+	result.verbs=returnSortedLowerCaseArray(result.verbs);
+	result.adjectives=returnSortedLowerCaseArray(result.adjectives);
+	result.adverbs=returnSortedLowerCaseArray(result.adverbs);
+	result.rest=returnSortedLowerCaseArray(result.rest);
+	data = refineArray(result);
+	doneParseRss=true;
 }
 
 function parseFeed(rssFeed) {
@@ -25,9 +96,15 @@ function parseFeed(rssFeed) {
 			}
 		}
 		for (var j = 0 ; j < articles.length ; j++ ) {	
+			if (articles[j].link.indexOf('/video/') != -1 ) continue;
+			//Parse title
+			wordpos.getPOS(articles[j].title,getRssParsedResult);
+			deasync.loopWhile(waitForFunctionParseRss);	
 			var paragraphs = parser.getParsedData(articles[j].link);
 			paragraphs=paragraphsFormater(paragraphs);
-			console.log(paragraphs);
+			doneParseRss=false;
+			console.log("Data : " +JSON.stringify(data));
+			data={};
 		}
 	});
 
