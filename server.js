b77fcb52c91a5fd8e8d8880defe62639fc814b86
Olga Yasovsky <oyasovsky@interwise.com>
2016-02-20 06:59:20 -0600

    display index html

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.redirect("/index.html");
 });
 
 app.listen(3003, function () {
