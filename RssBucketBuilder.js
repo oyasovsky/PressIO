b5902af3680946d75b067bc7b8deeacdc4b03fb4
danperetz <dan.peretz@gmail.com>
2016-02-20 08:55:34 -0600

    CNN RSS is parsed

--- RssBucketBuilder.js
+++ RssBucketBuilder.js
@@ -1,8 +1,15 @@
 var feed = require('feed-read');
-var rssFeedsArray = ['http://rss.cnn.com/rss/edition.rss'];
+var rssFeedsArray = [['http://rss.cnn.com/rss/edition.rss',require('./cnnParser')]];
 
-function parseFeed(url) {
-	feed(url,function(err,rawArticles) {
+function paragraphsFormater(pharagraphs) {
+	return pharagraphs;
+}
+
+function parseFeed(rssFeed) {
+	var feedUrl = rssFeed[0];
+	var parser = rssFeed[1];
+	feed(feedUrl,function(err,rawArticles) {
+		if (err) throw err;
 		//check for duplicated articles
 		var articles=[];
 		for (var i = 0 ; i < rawArticles.length; i++) {
@@ -17,15 +24,18 @@ function parseFeed(url) {
 				articles.push(rawArticles[i]);
 			}
 		}
-		
-		console.log(articles);
+		for (var j = 0 ; j < articles.length ; j++ ) {	
+			var paragraphs = parser.getParsedData(articles[j].link);
+			paragraphs=paragraphsFormater(paragraphs);
+			console.log(paragraphs);
+		}
 	});
 
 }
 
 function main() {
 	for (var i = 0; i <  rssFeedsArray.length; i++) {
-		parseFeed(rssFeedsArray);
+		parseFeed(rssFeedsArray[i]);
 	}
 }
 
