5cac56d53514edaf179e26831de078ba3cde13ec
Olga Yasovsky <oyasovsky@interwise.com>
2016-02-20 09:51:04 -0600

    add saveaudio and save video menus

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,36 @@
 var colors = ['#1abc9c',  '#57d68d', '#5cace2', '#9b59b6', '#f1c40f', '#e67e22', '#e74e60']
 angular.module('pressIO', [])
 	.controller('RSSTagsController', ['$scope', function ($scope) {
-    $scope.rssFeeds = [
-      "David Bowie",
-      "Turkey Bombing",
-      "Middle East Crisis",
-      "Oscar Nominations",
-      "World Poverty Report"
-    ];
+	$scope.rssFeeds = [
+      	"David Bowie",
+      	"Turkey Bombing",
+      	"Middle East Crisis",
+      	"Oscar Nominations",
+      	"World Poverty Report"
+    	];
+
+	$scope.setColor = function(index) {
+		var i = (index>=colors.length) ? index % colors.length : index;
+		return colors[i]; 
+	};
+
+
   }]);
 
-var pos = Math.floor(Math.random()*colors.length);
-
-setTimeout(function() {
-	console.log("timeout");
-	i=pos;
-	$("ul.rss-tags li").each(function() {
-		console.log("color: ", colors[i]);
-		$(this).css("background-color", colors[i++]);
-		if (i==colors.length) i=0;
+function ajaxExportAudio() {
+	var scope = angular.element(
+	document.getElementById("main")).scope();
+	scope.$apply(function () {
+		scope.saveAudio();
 	});
-}, 2000);
+}
+
+function ajaxExportVideo() {
+	var scope = angular.element(                                                                                                                                                         
+        document.getElementById("main")).scope();
+        scope.$apply(function () {
+                scope.saveVideo();
+        });
+}
+
 
